ec0aded218a3909d3c77b615d97f8abfb7b35a16
Grant Zou <grantbtzou@gmail.com>
2025-05-08 17:44:06 -0400

    Updates

--- component---src-pages-404-js-58cdc1baa47084772ae0.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[125],{3331:function(t,e,n){n.r(e),n.d(e,{Head:function(){return s}});var a=n(6540),u=n(6616),l=n(7528);const s=()=>a.createElement(l.A,{title:"404: Not Found"});e.default=()=>a.createElement(u.A,null,a.createElement("h1",null,"404: Not Found"),a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}]);
-//# sourceMappingURL=component---src-pages-404-js-58cdc1baa47084772ae0.js.map
